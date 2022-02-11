91ce0569665f13adae5f8663d684e5b110d1eb9b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2022-02-11 17:47:36 +0100

    Add files via upload

---
+++ ESP32_ES_LCD_YRNO_TUNNEL_0001/extra.h
@@ -0,0 +1,116 @@
+/*
+void listFileFromSPIFFS(){
+    unsigned int totalBytes = SPIFFS.totalBytes();
+    unsigned int usedBytes = SPIFFS.usedBytes();
+    Serial.println("\n===== File system info =====");
+    Serial.print("Total space: "); Serial.print(totalBytes); Serial.println(" byte");
+    Serial.print("Total used:  "); Serial.print(usedBytes); Serial.println(" byte");
+    Serial.println();
+  File root = SPIFFS.open("/");  
+  File file = root.openNextFile();
+  while(file){
+      Serial.print("FILE: "); Serial.print(file.name());
+      Serial.print("\t\t");   Serial.println(file.size(), DEC);      
+      file = root.openNextFile();
+  }
+}
+*/
+
+
+
+/*
+void readDATA(){ 
+    File file2 = SPIFFS.open("/streams.txt");
+ 
+    if(!file2){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+ 
+    Serial.println("File Content::::::::::::::::::::");
+ 
+    while(file2.available()){
+ 
+        Serial.write(file2.read());
+    }
+ 
+    file2.close();
+}
+
+
+void saveDATA(const String ParamValue){
+    Serial.println("#606 saveDATA==============");
+    Serial.println(ParamValue);
+    //savePreferences();
+                    File file = SPIFFS.open("/streams.txt", FILE_WRITE);
+                   
+                    if (!file) {
+                      Serial.println("There was an error opening the file for writing");
+                      return;
+                    }
+                    if (file.print(ParamValue)) {
+                      Serial.println("File was written++");
+                    } else {
+                      Serial.println("File write failed---");
+                    }
+                   
+                    file.close();    
+}
+*/
+
+
+
+
+/*
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+*/
+/*
+String getValue(String data,  int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)=='\n' || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+*/
+
+
+
+
+struct sQQQ  {
+  int8_t l;  
+  int8_t m;
+  int8_t h;
+};
+
+
+sQQQ qqq[5]={
+    { -2, 4, -2},
+    { 6, -2, 6},
+    { 6,-6, 6},
+    { 6,-9, 6},
+    { 0,0,0}
+};
